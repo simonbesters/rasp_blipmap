bd3b6cb951b75ed27d56d3652697c222e6ce4028
David Hofstee <david@c001.nl>
2019-12-23 09:30:40 +0100

    add NL1KM

--- download_binaries.sh
+++ download_binaries.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-files="NETHERLANDS.tar.gz  rasp-gm-NETHERLANDS.tar.gz geog.tar.gz rasp-gm-stable.tar.gz rangs.tgz"
+files="\
+NETHERLANDS.tar.gz \
+NL1KM-1.tar.gz \
+NL1KM-2.tar.gz \
+rasp-gm-NETHERLANDS.tar.gz \
+geog.tar.gz \
+geog.fine.tar.gz \
+rasp-gm-stable.tar.gz \
+rangs.tgz"
+
 BASE_URL="https://docker.blipmaps.nl/downloads"
 
 for f in $files ; do
